230d0e8074a23bbb34fb2000c34fa6b9fdc03b7a
Jed <jedster1111@hotmail.co.uk>
2018-07-17 18:22:53 +0100

    Got a very basic animation set up going

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import { Transition, animated } from 'react-spring';
+import React, {PureComponent} from 'react';
+import { Spring, animated } from 'react-spring';
 import './App.css';
 
-const pages = [
-  style => <animated.div style={{ ...style, background: '#247BA0' }}>A</animated.div>,
-  style => <animated.div style={{ ...style, background: '#B2DBBF' }}>B</animated.div>,
-  style => <animated.div style={{ ...style, background: '#12DBBF' }}>C</animated.div>
-]
+class ClickMe extends PureComponent {
+  constructor(props){
+    super(props);
+    this.state = {
+      toggle:true,
+    }
+  }
+  handleToggle = () => this.setState(state => ({ toggle: !state.toggle }));
 
-class App extends React.PureComponent {
-  state = { index: 0 }
-  toggle = e => this.setState(state => ({ index: state.index === 2 ? 0 : state.index + 1 }))
-  render() {
+  render(){
+    const isToggle = this.state.toggle;
+    const rotation = isToggle ? '0deg' : '150deg';
+    const translation = isToggle ? '0px,0px' : '50px, 100px';
+    const scale = isToggle ? '1, 1' : '1.5, 1.5';
+    return(
+      <Spring
+        native
+        from = {{ opacity:0 }}
+        to = {{
+          opacity: 1,
+          backgroundColor: isToggle ? 'yellow' : 'blue',
+          transform: `translate(${translation}) rotate(${rotation}) scale(${scale})`,
+        }}>
+        {styles => <animated.div style = {styles} className='test' onClick={this.handleToggle}>I will fade in</animated.div>}
+      </Spring>
+    )
+  }
+}
+
+class App extends PureComponent {
+  render(){
     return (
-      <div className="main" onClick={this.toggle}>
-        <Transition
-          native
-          from={{ opacity: 0, transform: 'translate3d(100%,0,0)' }}
-          enter={{ opacity: 1, transform: 'translate3d(0%,0,0)' }}
-          leave={{ opacity: 0, transform: 'translate3d(-50%,0,0)' }}>
-          {pages[this.state.index]}
-        </Transition>
+      <div id='main-content'>
+        <ClickMe />
+        <ClickMe />
       </div>
     )
   }
